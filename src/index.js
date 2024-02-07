25e29291b211dd657c17ff57b6b23d628a92514e
renox <planetrenox@protonmail.com>
2024-02-07 05:11:02 -0800

    Uploading now works and modal goes away after response.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 const {$, $$} = require('cute-socks');
+// const {dynamoDBClient} require('./util/awsConfig.js');
+// const {getLatestPosts} require('./util/dynamoDbUtils.js');
+const {compressImage} = require('./util/imageUtils.js');
+const {uploadSockToCloudFunction} = require('./util/googleCloud.js');
 
+const {getUriExtension, getFileNameFromUri} = require('cute-util');
+
+const USER_POSTS_TABLE = "girlsockdir"; // DynamoDB Table Name
 
 document.addEventListener('DOMContentLoaded', onDomContentLoad);
 
 
 function onDomContentLoad()
 {
-    $('#post-form-submit').addEventListener('click', function (event)
+    $('#post-form-submit').addEventListener('click', async function (event)
     {
         event.preventDefault();
         $('#post-form-submit-loading-modal').style.display = 'block';
-        submitPost();
+        let respo = await submitPost();
+        if (respo.ok)
+        {
+            $('#post-form-submit-loading-modal').style.display = 'none';
+        }
     });
 }
 
-function submitPost()
+async function submitPost()
 {
-    // TODO
+    const fileInput = $('#post-image');
+    const textInput = $('#post-body')?.value;
+
+    if (fileInput.files.length > 0)
+    {
+        const compressedImage = await compressImage(fileInput.files[0]);
+        const response = await uploadSockToCloudFunction('/th/', getFileNameFromUri(fileInput.files[0].name), compressedImage, textInput);
+        console.log(response.ok);
+        return response;
+
+    }
 }
