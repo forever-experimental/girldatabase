3b3c5c5b19090dfdff028bc30be2014917cdb8b0
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2024-02-14 17:33:13 -0500

    Update girl.js

--- js/components/girl.js
+++ js/components/girl.js
@@ -6,7 +6,7 @@ export const sock = (imgUri, imgFileName, imgRes, commentsCount, txt, id, timeSt
         <img class="image", id="img-${id}" src="${imgUri}" alt="user attached image" loading="lazy">
         <div class="meta">
             ${imgFileName} <span id="imgRes-${id}">(${imgRes})</span> /
-            <i>${commentsCount} comments</i> / ${timeStr}
+            <i>${timeStr}</i>
         </div>
         <div class="body">
             ${txt}
