a18050e03f25daadb73710422aa6c880daee7cdf
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2024-02-14 00:56:16 -0500

    Update girl.js

--- src/components/girl.js
+++ src/components/girl.js
@@ -3,7 +3,7 @@ import {html} from 'lit-html';
 
 export const sock = (imgUri, imgFileName, imgRes, imgSize, commentsCount, txt) => html`
     <article>
-        <img class="image" onclick="alert('wip')" src="${imgUri}" alt="user attached image" loading="lazy">
+        <img class="image" src="${imgUri}" alt="user attached image" loading="lazy">
         <div class="meta">
             ${imgFileName} (${imgRes}, ${imgSize}) / <i>${commentsCount} comments</i>
         </div>
