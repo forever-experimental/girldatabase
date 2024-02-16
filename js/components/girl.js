70efa193503c37c582d875d733277b1eb0e102b8
kitsumitsu <75032462+BlueBunnex@users.noreply.github.com>
2024-02-15 23:01:19 -0500

    Update girl.js

--- js/components/girl.js
+++ js/components/girl.js
@@ -5,8 +5,7 @@ export const sock = (imgUri, imgFileName, imgRes, commentsCount, txt, id, timeSt
         <img class="image", id="img-${id}" src="${imgUri}" alt="user attached image" loading="lazy">
         <div class="meta">
             <span style="max-width: 50%; display: inline-block; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${imgFileName}</span>
-            <span id="imgRes-${id}">(${imgRes})</span> /
-            <i>${timeStr}</i>
+            <span style="display: inline-block; overflow: hidden;"><span id="imgRes-${id}">(${imgRes})</span> / <i>${timeStr}</i></span>
         </div>
         <div class="body">
             ${txt}
