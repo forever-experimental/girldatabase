2b1dbec536af97ddb19aee2823e917ad668fa86e
reno­‎­‌⁠‏­‌⁠‏­‌⁠‍​‌⁠‏‏‌‏⁠​‌‏‏‎‌‏‏‎‌⁠‏‏‌⁠‍⁠‌⁠‏⁠‌⁠‍­‌­​​‌­​⁠‌⁠‍‍‌⁠‍⁠‌⁠‏⁠‌­​​‌‏‏​‌­​⁠‌⁠‍⁠‌⁠‍‏‌‏‏‎‌⁠‏­‌­​‎‌­‎‎‌⁠‍‍‌­‎⁠‌⁠‍­‌⁠‏­‌⁠‍⁠‌⁠‏‎‌­​‎‌‏‏‎‌­‎⁠‌­​​‌⁠‍‍‌­​‎‌‏‏⁠‌­‎­‌­​‏‌⁠‍­‌­​​‌⁠‏‏x <planetrenox@protonmail.com>
2024-03-12 08:36:06 -0700

    Update socks.index.js

--- src/js/frontend/socks.index.js
+++ src/js/frontend/socks.index.js
@@ -77,7 +77,7 @@ async function submitPost()
 
 const CuteLoadingModal = (() => {
     const modalId = 'post-form-submit-loading-modal';
-    const getModalHtml = () => html`
+    const getModalHtml = () => `
         <div id="${modalId}" style="position:fixed; left:0; top:0; width:100%; height:100%; background:rgba(0,0,0,0.5);">
             <div style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%); padding:20px; background:#fff;">
                 Loading....
