c31529c7b048bdd161b64ea92ed56fa638be30bd
reno­‎­‌⁠‏­‌⁠‏­‌⁠‍​‌⁠‏‏‌‏⁠​‌‏‏‎‌‏‏‎‌⁠‏‏‌⁠‍⁠‌⁠‏⁠‌⁠‍­‌­​​‌­​⁠‌⁠‍‍‌⁠‍⁠‌⁠‏⁠‌­​​‌‏‏​‌­​⁠‌⁠‍⁠‌⁠‍‏‌‏‏‎‌⁠‏­‌­​‎‌­‎‎‌⁠‍‍‌­‎⁠‌⁠‍­‌⁠‏­‌⁠‍⁠‌⁠‏‎‌­​‎‌‏‏‎‌­‎⁠‌­​​‌⁠‍‍‌­​‎‌‏‏⁠‌­‎­‌­​‏‌⁠‍­‌­​​‌⁠‏‏x <planetrenox@protonmail.com>
2024-03-31 20:02:55 -0700

    Update girl.js

--- src/js/frontend/components/girl.js
+++ src/js/frontend/components/girl.js
@@ -1,4 +1,4 @@
-import { html } from 'cute-html';
+import { html } from 'https://cdn.jsdelivr.net/npm/cute-html@1.0.1';
 
 export const sock = (imgUri, imgFileName, imgRes, commentsCount, txt, id, timeStr) => html`
     <article>
