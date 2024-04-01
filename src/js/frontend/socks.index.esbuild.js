a11c46a850b949edc68b63a73afa25183aadeef4
reno­‎­‌⁠‏­‌⁠‏­‌⁠‍​‌⁠‏‏‌‏⁠​‌‏‏‎‌‏‏‎‌⁠‏‏‌⁠‍⁠‌⁠‏⁠‌⁠‍­‌­​​‌­​⁠‌⁠‍‍‌⁠‍⁠‌⁠‏⁠‌­​​‌‏‏​‌­​⁠‌⁠‍⁠‌⁠‍‏‌‏‏‎‌⁠‏­‌­​‎‌­‎‎‌⁠‍‍‌­‎⁠‌⁠‍­‌⁠‏­‌⁠‍⁠‌⁠‏‎‌­​‎‌‏‏‎‌­‎⁠‌­​​‌⁠‍‍‌­​‎‌‏‏⁠‌­‎­‌­​‏‌⁠‍­‌­​​‌⁠‏‏x <planetrenox@protonmail.com>
2024-04-01 07:22:39 -0700

    Update socks.index.esbuild.js

--- src/js/frontend/socks.index.esbuild.js
+++ src/js/frontend/socks.index.esbuild.js
@@ -1,16 +1,7 @@
 (() => {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __getProtoOf = Object.getPrototypeOf;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __esm = (fn, res) => function __init() {
-    return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
-  };
-  var __commonJS = (cb, mod) => function __require() {
-    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
-  };
+  var 
   var __export = (target, all) => {
     for (var name in all)
       __defProp(target, name, { get: all[name], enumerable: true });
